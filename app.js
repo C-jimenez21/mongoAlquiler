afe032c64da7e899fd59912686b6130a8c446360
C-jimenez21 <cristianjjc21@gmail.com>
2023-08-08 17:44:00 -0500

    feat: :sparkles: Actualizacion del router campues agregando algunos middlewares

--- app.js
+++ app.js
@@ -1,11 +1,14 @@
 import express from 'express';
 import dotenv from 'dotenv';
 import appCampus from './routers/campus.js';
+import {appToken, appVerify} from './limit/token.js';
+
 const appExpress = express();
 dotenv.config()
 
 appExpress.use(express.json());
-appExpress.use('/campus', appCampus);
+appExpress.use('/campus', appVerify, appCampus);
+appExpress.use('/token', appToken);
 
 let config = JSON.parse(process.env.MY_SERVER);
 
--- routers/campus.js
+++ routers/campus.js
@@ -1,30 +1,24 @@
-import { ObjectId } from 'mongodb';
-import { con } from '../db/mongodb.js';
 import {limitGet} from '../limit/config.js';
 import { Router } from 'express';
+import appMiddlewareCampusVerify from '../middlewares/campus.middleware.js';
+
 const appCampus = Router();
 
-appCampus.get('/', limitGet(),async(req, res) => {
-    if(!req.rateLimit)return;
-    let {id} = req.body;
-    let db = await con();
-    let user = db.collection('user');
-    let result = await user.find({_id: new ObjectId(id)}).toArray();
-    res.send(result);
+appCampus.get('/', limitGet(), async(req, res) => {
+    // if(!req.rateLimit)return;
+    // let {id} = req.body;
+    // let db = await con();
+    // let user = db.collection('user');
+    // let result = await user.find({_id: new ObjectId(id)}).toArray();
+    // res.send(result);
+res.send("entre a GET campus");
+
 });
 
-appCampus.post('/', limitGet(),async(req, res) => {
-    if(!req.rateLimit)return;
-    let db = await con();
-    let user = db.collection('user');
-    try {
-        let result = await user.insertOne(req.body);
-        console.log(result);
-        res.send(result);
-    } catch (error) {
-        console.log(error.errInfo.details.schemaRulesNotSatisfied[0]);
-        res.send("Something went wrong")
-    }
+appCampus.post('/', limitGet(), appMiddlewareCampusVerify, async(req, res) => {
+
+    
+    res.send("Entre al POST");
 });
 
 
